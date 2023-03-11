788330af77532c75b4c0ac8e2039f2da97df6a39
SSzrEdeRR <99695092+SSzrEdeRR@users.noreply.github.com>
2023-03-11 15:17:15 +0100

    11.03.2023 zadania

---
+++ main.js
@@ -0,0 +1,52 @@
+/*
+function ocena(ocena_ucznia) {
+const ocena = Number (prompt("podaj ocene"))
+
+if (ocena == 1) {
+    console.log("Ocena Niedostateczna")
+    return ocena_ucznia
+}
+else if (ocena == 2 ) {
+    console.log("Ocena Dospuszczająca")
+    return ocena_ucznia
+}
+else if (ocena == 3 ) {
+    console.log("Ocena Dostateczna")
+    return ocena_ucznia
+}
+else if (ocena == 4 ) {
+    console.log("Ocena Dobra")
+    return ocena_ucznia
+}
+else if (ocena == 5 ) {
+    console.log("Ocean Bardzo Dobra")
+    return ocena_ucznia
+}
+else if (ocena == 6 ) {
+    console.log("Ocena Celująca")
+    return ocena_ucznia
+}
+else {
+    console.log("Nie ma takiej wartosci")
+    }
+}
+*/
+
+/*
+function mnozenie (a,a,a,a,a,a,a,a,a,a) {
+    return a*a*a*a*a*a*a*a*a*a
+}
+console.log( mnozenie(10, 10, 10, 10, 10, 10, 10, 10, 10, 10))
+*/
+
+/*
+const liczba = Number( prompt("Podaj cyfre") );
+
+if (liczba % 2 === 0 ) {
+    console.log("liczba jest parzysta")
+}
+else {
+    console.log("liczba jest nieparzysta")
+}
+*/
+
